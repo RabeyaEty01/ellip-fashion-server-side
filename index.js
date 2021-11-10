8eaec27d2ec311b3381f2ad40955fdf474888b5a
RabeyaEty01 <etyrabeya01@gmail.com>
2021-11-11 02:23:39 +0600

    done

--- index.js
+++ index.js
@@ -26,15 +26,55 @@ async function run() {
         const database = client.db("EllipFashion");
         const productsCollection = database.collection("products");
         const reviewCollection = database.collection("reviews");
-       
-        
+        const ordersCollection = database.collection("orders");
+
+
         //GET ALL Products
         app.get('/products', async (req, res) => {
             const result = await productsCollection.find({}).toArray();
             res.send(result);
         });
 
-    
+        //GET ALL Reviews
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find({}).toArray();
+            res.send(result);
+        });
+
+
+
+        //GET Single Product
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            console.log('load single product with id', id);
+            res.send(product);
+        });
+
+        //GET My Orders
+        app.get('/myOrders/:email', async (req, res) => {
+            const result = await ordersCollection.find({ email: req.params.email }).toArray();
+            res.send(result);
+        })
+
+        //Place Order 
+        app.post('/placeOrder', (req, res) => {
+            console.log(req.body);
+            ordersCollection.insertOne(req.body).then(result => {
+                res.send(result);
+            });
+        })
+
+        //GET All Orders
+        app.get('/orders', async (req, res) => {
+            const result = await ordersCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        
+
+
         //POST Product API
         app.post('/addProducts', async (req, res) => {
             const product = req.body;
@@ -53,7 +93,19 @@ async function run() {
             res.json(result);
         });
 
-    
+
+           //DELETE Order
+           app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            console.log(result);
+
+             console.log('deleteing user with id', id);
+
+            res.json(result);
+        });
+
     }
     finally {
         //await client.close();
